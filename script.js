8c4645f41c19cbdb41665d1676dee0d60e89e231
mbarbosasan <moisesbarbosa23@gmail.com>
2022-12-17 19:25:24 -0300

    add: day 2.1

--- script.js
+++ script.js
@@ -1,26 +1,69 @@
-let resultado = {
-  soma: 0,
-  numero: 0,
-}
+let pontuacao = 0;
 
 let arrayNumber = new Array();
 const fetchApi = fetch("./dados.txt")
 .then((response) => response.text())
 .then((json) => {
-  dados = JSON.stringify(json).replaceAll("\\r\\n\\r\\n", ",").replaceAll("\\n", ":").replaceAll("\\r", "").replaceAll('"', "").split(",");
-  dados.forEach((item, index) => {
-    let soma = 0;
-    arrSum = item.split(":");
-    arrayNumber.push(arrSum.reduce((acc, a) => {
-      return acc + Number(a)
-    }, 0));
-
-  })
-  console.log(resultado);
-  arrayNumber.sort((a, b) => a - b).reverse()
-  console.log(arrayNumber[0] + arrayNumber[1] + arrayNumber[3]);
+  dados = JSON.stringify(json)
+  .replaceAll(" ", ":")
+  .replaceAll("\\r\\n", ",")
+  .replaceAll("/", "")
+  .replaceAll('"', "")
+  .replaceAll("A", "rock")
+  .replaceAll("B", "paper")
+  .replaceAll("C", "scissors")
+  .replaceAll("X", "rock")
+  .replaceAll("Y", "paper")
+  .replaceAll("Z", "scissors")
+  .split(",")
+  dados.map((item) => {
+    jogo = item.split(":")
+    jogadorA = jogo[0];
+    jogadorB = jogo[1];
+    if (jogadorB === "scissors") {
+      pontuacao += scissorsAgainst(jogadorA)
+    } else if (jogadorB === "rock") {
+      pontuacao += rockAgainst(jogadorA)
+    } else {
+      pontuacao += paperAgainst(jogadorA)
+    }
+  }
+  )
+  console.log(pontuacao);
 })
 
+// A = PEDRA, B = PAPER, C = TESOURA
+// X = PEDRA, Y = PAPER, C = TESOURA
+
+function scissorsAgainst(option) {
+  if (option === "rock") {
+    return 3 + 0
+  } else if (option === "paper") {
+    return 3 + 6
+  } else {
+    return 3 + 3
+  }
+}
+
+function rockAgainst(option) {
+  if (option === "scissors") {
+    return 1 + 6
+  } else if (option === 'paper') {
+    return 1 + 0
+  } else {
+    return 1 + 3
+  }
+}
+
+function paperAgainst(option) {
+  if (option === "scissors") {
+    return 2 + 0
+  } else if (option === "rock") {
+    return 2 + 6
+  } else {
+    return 2 + 3;
+  }
+}
 
 
 
