45476467bff1abfe4e7b0abb376ab735e1bcb4b0
mbarbosasan <moisesbarbosa23@gmail.com>
2022-12-19 21:52:03 -0300

    day 3: #1

--- script.js
+++ script.js
@@ -1,88 +1,26 @@
-let pontuacao = 0;
 
-let arrayNumber = new Array();
+// https://adventofcode.com/2022/day/3
+
+let numberPriority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numberPriority = numberPriority.split("")
+
+let sum = 0
+
 const fetchApi = fetch("./dados.txt")
 .then((response) => response.text())
 .then((json) => {
-  dados = JSON.stringify(json)
-  .replaceAll(" ", ":")
-  .replaceAll("\\r\\n", ",")
-  .replaceAll("/", "")
-  .replaceAll('"', "")
-  .replaceAll("A", "rock")
-  .replaceAll("B", "paper")
-  .replaceAll("C", "scissors")
-  .replaceAll("X", "rock")
-  .replaceAll("Y", "paper")
-  .replaceAll("Z", "scissors")
-  .split(",")
-  dados.forEach((item) => {
-    jogo = item.split(":")
-    jogadorA = jogo[0];
-    situacao = jogo[1];
-    if (situacao === "scissors") {
-      if (jogadorA === "scissors") {
-        pontuacao += rockAgainst(jogadorA);
-      } else if (jogadorA === "rock") {
-        pontuacao += paperAgainst(jogadorA)
-      } else {
-        pontuacao += scissorsAgainst(jogadorA)
-      }
-    } else if (situacao === "rock") {
-      if (jogadorA === "scissors") {
-        pontuacao += paperAgainst(jogadorA);
-      } else if (jogadorA === "rock") {
-        pontuacao += scissorsAgainst(jogadorA);
-      } else {
-        pontuacao += rockAgainst(jogadorA)
-      }
-    } else {
-      if (jogadorA === "scissors") {
-        pontuacao += scissorsAgainst(jogadorA)
-      } else if (jogadorA === "rock") {
-        pontuacao += rockAgainst(jogadorA)
-      } else {
-        pontuacao += paperAgainst(jogadorA)
-      }
-    }
-  }
-  )
-  console.log(pontuacao);
+  dados = JSON.stringify(json).replaceAll('"', "").replaceAll("\\r\\n", ",").split(",")
+  dados.forEach((sacola) => {
+    let firstHalf = Array.from(sacola).slice().splice(0, Math.ceil(sacola.length / 2))
+    let lastHalf = Array.from(sacola).slice().splice(-Math.ceil(sacola.length / 2))
+    let sameValueInArray = firstHalf.filter((item) => lastHalf.includes(item));
+    sum += numberPriority.findIndex((item) => sameValueInArray.includes(item))
+    sum++
+    })
+    console.log(sum)
 })
 
-// A = PEDRA, B = PAPER, C = TESOURA
-// X = PEDRA, Y = PAPER, C = TESOURA
-// ROCK = PERDER, PAPER = EMPATAR, TESOURA = GANHAR
 
-function scissorsAgainst(option) {
-  if (option === "rock") {
-    return 3 + 0
-  } else if (option === "paper") {
-    return 3 + 6
-  } else {
-    return 3 + 3
-  }
-}
-
-function rockAgainst(option) {
-  if (option === "scissors") {
-    return 1 + 6
-  } else if (option === 'paper') {
-    return 1 + 0
-  } else {
-    return 1 + 3
-  }
-}
-
-function paperAgainst(option) {
-  if (option === "scissors") {
-    return 2 + 0
-  } else if (option === "rock") {
-    return 2 + 6
-  } else {
-    return 2 + 3;
-  }
-}
 
 
 
